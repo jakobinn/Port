03330ee88337c137440fca8267f98b843b78ba09
Jakob <jobbi1991@gmail.com>
2018-10-19 00:13:19 +0000

    unnecessary content deleted

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,13 @@
-/*$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-});*/
-
-$(function () {
-	var filterList = {
-		init: function () {
-			$('#portitems').mixItUp({
-				selectors: {
-			  target: '.portfolio',
-			  filter: '.filter'
-		  },
-		 /* load: {
-  		  filter: '.website'
-  		}*/
-			});
-		}
-	};
-	// Run the show!
-	filterList.init();
+$(function() {
+    var filterList = {
+        init: function() {
+            $('#portitems').mixItUp({
+                selectors: {
+                    target: '.portfolio',
+                    filter: '.filter'
+                },
+            });
+        }
+    };
+    filterList.init();
 });
